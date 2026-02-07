c7493095a5c014d5ebd2c0f73b8c5153876dda00
mithun50 <mithungowda.b7411@gmail.com>
2026-02-07 11:46:37 +0530

    Fix rootfs download URL: 24.04.2 → 24.04.3
    The Ubuntu base image version was 24.04.2 which no longer exists.
    Updated to 24.04.3 which is the current release.
    
    Co-Authored-By: Mithun Gowda B <mithungowda.b7411@gmail.com>

--- flutter_app/lib/constants.dart
+++ flutter_app/lib/constants.dart
@@ -8,7 +8,7 @@ class AppConstants {
   static const String gatewayUrl = 'http://$gatewayHost:$gatewayPort';
 
   static const String ubuntuRootfsUrl =
-      'https://cdimage.ubuntu.com/ubuntu-base/releases/24.04/release/ubuntu-base-24.04.2-base-';
+      'https://cdimage.ubuntu.com/ubuntu-base/releases/24.04/release/ubuntu-base-24.04.3-base-';
   static const String rootfsArm64 = '${ubuntuRootfsUrl}arm64.tar.gz';
   static const String rootfsArmhf = '${ubuntuRootfsUrl}armhf.tar.gz';
   static const String rootfsAmd64 = '${ubuntuRootfsUrl}amd64.tar.gz';
